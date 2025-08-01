e2e0ac841813f0899c2fbc7ac7e44fa1a7cab560
zucoloto <zucoloto.alessandro@gmail.com>
2025-08-01 17:36:14 -0300

    Padrão de Projetos DTO-Data Transfer Object-Início

---
+++ 09-rest-with-spring-boot-and-java-erudio/src/main/java/br/com/zuco/services/PersonServices.java
@@ -0,0 +1,55 @@
+package br.com.zuco.services;
+
+import br.com.zuco.exception.ResourceNotFoundException;
+import br.com.zuco.model.Person;
+import br.com.zuco.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    @Autowired
+    PersonRepository repository;
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding all People!");
+        return repository.findAll();
+    }
+
+    public Person findById(Long id) {
+        logger.info("Finding one Person!");
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+        return repository.save(person);
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+        return repository.save(person);
+    }
+
+    public void delete(Long id) {
+        logger.info("Deleting one Person!");
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        repository.delete(entity);
+    }
+}
