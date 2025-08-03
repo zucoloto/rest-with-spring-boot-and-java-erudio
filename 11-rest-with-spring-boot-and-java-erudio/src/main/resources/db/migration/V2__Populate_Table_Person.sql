29737c4ad958a81478e7be3e5e35cdef26150779
zucoloto <zucoloto.alessandro@gmail.com>
2025-08-03 20:55:32 -0300

    Migrations com Flyway

---
+++ 11-rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - BR', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Rio de Janeiro - BR', 'Leonardo', 'Male', 'da Vinci'),
+	(3, 'Espírito Santo - BR', 'Indira', 'Female', 'Gandhi'),
+	(4, 'Minas Gerais - BR', 'Mahatma', 'Male', 'Gandhi'),
+	(5, 'Distrito Federal - BR', 'Muhamamd', 'Male', 'Ali'),
+	(6, 'Paraná - BR', 'Nelson', 'Male', 'Mandela'),
+	(7, 'Tocantins - BR', 'Nelson', 'Male', 'Mandela'),
+	(8, 'Pará - BR', 'Nikola', 'Male', 'Tesla'),
+	(9, 'Bahia - BR', 'Nikola', 'Male', 'Tesla');
